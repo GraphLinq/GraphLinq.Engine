1bf7bd00861baecc1081bd9a8adc4e00e11229a5
Nightwolf <nico.style931@gmail.com>
2021-04-10 02:03:40 +0200

    Add date blocks New Json encoding blocks Functions methods Timer node will now trigger at the beginning

---
+++ Nodes/Date/FormatDateNode.cs
@@ -0,0 +1,37 @@
+﻿using NodeBlock.Engine.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NodeBlock.Engine.Nodes.Date
+{
+    [NodeDefinition("FormatDateNode", "Format Date", NodeTypeEnum.Function, "Time")]
+    [NodeGraphDescription("Format a date with a given pattern")]
+    public class FormatDateNode : Node
+    {
+        public FormatDateNode(string id, BlockGraph graph)
+            : base(id, graph, typeof(FormatDateNode).Name)
+        {
+            this.InParameters = new Dictionary<string, NodeParameter>()
+            {
+                { "date", new NodeParameter(this, "date", typeof(object), true) },
+                { "format", new NodeParameter(this, "format", typeof(string), true) }
+            };
+
+            this.OutParameters.Add("dateString", new NodeParameter(this, "dateString", typeof(string), false));
+        }
+
+        public override bool CanExecute => false;
+        public override bool CanBeExecuted => false;
+
+        public override object ComputeParameterValue(NodeParameter parameter, object value)
+        {
+            if (parameter.Name == "dateString")
+            {
+                var date = this.InParameters["date"].GetValue() as DateTime?;
+                return date.Value.ToString(this.InParameters["format"].GetValue().ToString());
+            }
+            return base.ComputeParameterValue(parameter, value);
+        }
+    }
+}
--- Nodes/Math/RoundNode.cs
+++ Nodes/Math/RoundNode.cs
@@ -1,4 +1,5 @@
-﻿using NodeBlock.Engine.Attributes;
+﻿
+using NodeBlock.Engine.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Text;
